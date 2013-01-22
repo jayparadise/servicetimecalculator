626ac281bad8e739cc31b3ac8c8352bbeaed21a2
jayparadise <jasonrparadise@gmail.com>
2013-01-22 17:01:32 -0500

    Update service_time.rb

--- service_time.rb
+++ service_time.rb
@@ -7,7 +7,6 @@ puts "Previous days of service time?"
 puts "Days into the season at callup?"
 	current = gets.chomp
 
-#proj_current_days = 183 - current.to_i
 if 183 - current.to_i >= 173
 	proj_current_days = 172
 else
@@ -15,8 +14,7 @@ else
 end
 
 total_days = previous_days.to_i + proj_current_days.to_i
-#new_days = total_days % 172
-#days = new_days.to_i % 172
+
 
 days = total_days.to_i % 172
 years = (total_days.to_i / 172) + previous_years.to_i
